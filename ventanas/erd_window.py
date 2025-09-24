42bb072eaa2f1625048c136ba39e32620cba07de
tommy-aki <141044473+tommy-aki@users.noreply.github.com>
2025-09-23 23:12:17 -0600

    Avances erd
    NOTA: Probablemente cambiar Graphviz por mermaid_py, ya que no me gustan los resultados

--- ventanas/erd_window.py
+++ ventanas/erd_window.py
@@ -1,9 +1,17 @@
+import os
+from pathlib import Path
 from db.erd import gen_erd
 from graphviz import Digraph
+import graphviz
 
-def gen_erd_window(conexion, filename="er_diagram.png"):
+def gen_erd_window(conexion, filename="er_diagram"):
+    # Ruta relativa al ejecutable dentro del proyecto
+    dot_dir = Path(__file__).parent.parent / "graphviz" / "bin"
+    os.environ["PATH"] = str(dot_dir) + os.pathsep + os.environ["PATH"]
+
+
     dot = Digraph(comment = f"ERD para {conexion}", format = "png")
-    dot.attr('node', shape='box')
+    dot.attr('node', shape='box') 
     fks = gen_erd(conexion)
     for _, refs in fks.items():
         for ref in refs:
